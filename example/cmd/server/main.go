f8b145262af729f9ca2de4effcde9adb1cabd844
Herb <ghstahl@gmail.com>
2021-09-06 08:56:37 -0400

    cleanup

--- example/cmd/server/main.go
+++ example/cmd/server/main.go
@@ -2,20 +2,14 @@
 package main
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/fluffy-bunny/grpcdotnetgo/example/internal"
-	pb "github.com/fluffy-bunny/grpcdotnetgo/example/internal/grpcContracts/helloworld"
 	_ "github.com/fluffy-bunny/grpcdotnetgo/example/internal/plugin"
-	middleware_grpc_auth "github.com/fluffy-bunny/grpcdotnetgo/pkg/middleware/auth"
-	grpcDIProtoError "github.com/fluffy-bunny/grpcdotnetgo/pkg/proto/error"
 	runtime "github.com/fluffy-bunny/grpcdotnetgo/pkg/runtime"
 	pkg "github.com/fluffy-bunny/protoc-gen-go-di/pkg"
 	"github.com/gogo/protobuf/gogoproto"
 	_ "github.com/jnewmano/grpc-json-proxy/codec"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 var version = "development"
@@ -38,23 +32,6 @@ func main() {
 
 }
 
-func exampleAuthFunc(ctx context.Context, fullMethodName string) (context.Context, interface{}, error) {
-
-	token, err := middleware_grpc_auth.AuthFromMD(ctx, "bearer")
-	if err != nil || token == "" {
-		replyFunc := pb.Get_helloworldFullEmptyResponseWithErrorFromFullMethodName(fullMethodName)
-		if replyFunc != nil {
-			reply := replyFunc()
-			replyError, ok2 := reply.(grpcDIProtoError.IError)
-			if ok2 {
-				myError := replyError.GetError()
-				myError.Code = 401
-				myError.Message = "Unauthorized"
-				return ctx, reply, status.Error(codes.Unauthenticated, "Unauthorized")
-			}
-		}
-		return ctx, nil, status.Error(codes.Unauthenticated, "Unauthorized")
-	}
-
-	return ctx, nil, nil
-}
+// Wondering where your grpc app actually is?
+// we are a plugin framework
+// internal.plugin.plugin.go  look for func init()
