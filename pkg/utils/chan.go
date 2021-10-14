2a57a7ebc9aeb159338934c36c7ad6f2d21d47bb
Herb <ghstahl@gmail.com>
2021-10-14 09:57:37 -0700

    add provision to unit test the grpc server using bufconn

--- pkg/utils/chan.go
+++ pkg/utils/chan.go
@@ -6,16 +6,27 @@ import (
 	"syscall"
 )
 
+var globalWaitChannel chan os.Signal
+
+// SignalQuit used for testing
+func SignalQuit() {
+	if globalWaitChannel != nil {
+		globalWaitChannel <- os.Interrupt
+	}
+}
+
+// WaitSignal waits for a syscall.X
 func WaitSignal() os.Signal {
-	ch := make(chan os.Signal, 2)
+	globalWaitChannel = make(chan os.Signal)
 	signal.Notify(
-		ch,
+		globalWaitChannel,
+		os.Interrupt,
 		syscall.SIGINT,
 		syscall.SIGQUIT,
 		syscall.SIGTERM,
 	)
 	for {
-		sig := <-ch
+		sig := <-globalWaitChannel
 		switch sig {
 		case syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM:
 			return sig
