b1bae7cc85a7e77f5c90537d672703a9a703dc8d
herb <ghstahl@gmail.com>
2022-03-31 09:53:56 -0700

    added runtime logic

--- go.mod
+++ go.mod
@@ -21,7 +21,9 @@ require (
 	github.com/gorilla/securecookie v1.1.1
 	github.com/gorilla/sessions v1.2.1
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
+	github.com/jedib0t/go-pretty/v6 v6.3.0
 	github.com/jnewmano/grpc-json-proxy v0.0.6
+	github.com/labstack/echo-contrib v0.12.0
 	github.com/labstack/echo/v4 v4.7.2
 	github.com/lestrrat-go/jwx v1.2.20
 	github.com/reugn/async v0.0.0-20200819063434-15e5b3951cd7
@@ -35,6 +37,7 @@ require (
 	github.com/spf13/viper v1.10.1
 	github.com/stretchr/testify v1.7.0
 	github.com/tkuchiki/parsetime v0.3.0
+	github.com/ziflex/lecho v1.2.0
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
 	google.golang.org/grpc v1.44.0
 	google.golang.org/protobuf v1.27.1
@@ -46,12 +49,15 @@ require (
 	cloud.google.com/go v0.99.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.0-20210816181553-5444fa50b93d // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/fatih/structtag v1.2.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/goccy/go-json v0.9.4 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
 	github.com/lestrrat-go/blackmagic v1.0.0 // indirect
@@ -61,10 +67,12 @@ require (
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
@@ -78,6 +86,7 @@ require (
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20211208223120-3a66f561d7aa // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
--- pkg/echo/runtime/runtime.go
+++ pkg/echo/runtime/runtime.go
@@ -1,1 +1,188 @@
 package runtime
+
+import (
+	contracts_container "github.com/fluffy-bunny/grpcdotnetgo/pkg/echo/contracts/container"
+	contracts_handler "github.com/fluffy-bunny/grpcdotnetgo/pkg/echo/contracts/handler"
+	services_contextaccessor "github.com/fluffy-bunny/grpcdotnetgo/pkg/echo/services/contextaccessor"
+	services_logger "github.com/fluffy-bunny/grpcdotnetgo/pkg/echo/services/logger"
+
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	echo_contracts_startup "github.com/fluffy-bunny/grpcdotnetgo/pkg/echo/contracts/startup"
+
+	"github.com/jedib0t/go-pretty/v6/table"
+
+	services_timeutils "github.com/fluffy-bunny/grpcdotnetgo/pkg/services/timeutils"
+
+	core_middleware_session "github.com/fluffy-bunny/grpcdotnetgo/pkg/echo/middleware/session"
+
+	middleware_logger "github.com/fluffy-bunny/grpcdotnetgo/pkg/echo/middleware/logger"
+
+	"github.com/fluffy-bunny/grpcdotnetgo/pkg/core"
+	contracts_session "github.com/fluffy-bunny/grpcdotnetgo/pkg/echo/contracts/session"
+	middleware_container "github.com/fluffy-bunny/grpcdotnetgo/pkg/echo/middleware/container"
+	services_cookies "github.com/fluffy-bunny/grpcdotnetgo/pkg/echo/services/cookies"
+	core_echo_templates "github.com/fluffy-bunny/grpcdotnetgo/pkg/echo/templates"
+	di "github.com/fluffy-bunny/sarulabsdi"
+	"github.com/google/uuid"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/pkgerrors"
+	"github.com/ziflex/lecho"
+)
+
+type (
+	Runtime struct {
+		Startup    echo_contracts_startup.IStartup
+		Container  di.Container
+		e          *echo.Echo
+		instanceID string
+	}
+)
+
+func New(startup echo_contracts_startup.IStartup) *Runtime {
+	return &Runtime{
+		Startup:    startup,
+		instanceID: uuid.New().String(),
+	}
+}
+func (s *Runtime) GetContainer() di.Container {
+	return s.Container
+}
+func (s *Runtime) Run() error {
+	startupOptions := s.Startup.GetOptions()
+	configOptions := s.Startup.GetConfigOptions()
+	err := core.LoadConfig(configOptions)
+	if err != nil {
+		return err
+	}
+
+	if configOptions.PrettyLog {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	}
+
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	switch strings.ToLower(configOptions.LogLevel) {
+	case "debug":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case "info":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case "warn":
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	case "error":
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	case "fatal":
+		zerolog.SetGlobalLevel(zerolog.FatalLevel)
+	case "panic":
+		zerolog.SetGlobalLevel(zerolog.PanicLevel)
+	case "trace":
+		zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	}
+	builder, _ := di.NewBuilder(di.App, di.Request, "transient")
+	err = s.AddDefaultServices(builder)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to add default services")
+	}
+	err = s.Startup.ConfigureServices(builder)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to configure services")
+	}
+	s.Container = builder.Build()
+	s.Startup.SetContainer(s.Container)
+	s.e = echo.New()
+	//use our own zerolog logger
+	s.e.Logger = lecho.New(os.Stdout)
+	//Set Renderer
+	s.e.Renderer = core_echo_templates.GetTemplateRender("./templates")
+
+	// MIDDELWARE
+	//-------------------------------------------------------
+	s.e.Use(middleware_logger.EnsureContextLogger(s.Container))
+	s.e.Use(middleware_logger.EnsureContextLoggerCorrelation(s.Container))
+	s.e.Use(middleware_container.EnsureScopedContainer(s.Container))
+	s.e.Use(session.Middleware(s.Startup.GetSessionStore()))
+	mainSession := contracts_session.GetGetSessionFromContainer(s.Container)
+	s.e.Use(core_middleware_session.EnsureSlidingSession(s.Container, mainSession))
+
+	if configOptions.ApplicationEnvironment == "Development" {
+		// this wipes out the session if we have a mismatch
+		s.e.Use(core_middleware_session.EnsureDevelopmentSession(s.Container, mainSession, s.instanceID))
+	}
+	app := s.e.Group("")
+	app.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+		TokenLookup:    "header:X-Csrf-Token,form:csrf",
+		CookiePath:     "/",
+		CookieSecure:   true,
+		CookieHTTPOnly: true,
+		CookieSameSite: http.SameSiteStrictMode,
+	}))
+
+	// we have all our required upfront middleware running
+	// now we can add the optional startup ones.
+	s.Startup.Configure(s.e, s.Container)
+
+	// our middleware that runs at the end
+	//-------------------------------------------------------
+	s.e.Use(middleware.Recover())
+	s.Startup.RegisterStaticRoutes(s.e)
+
+	// register our handlers
+	handlerFactory := contracts_handler.GetIHandlerFactoryFromContainer(s.Container)
+	handlerFactory.RegisterHandlers(app)
+	handlerDefinitions := contracts_handler.GetIHandlerDefinitions(s.Container)
+
+	t := table.NewWriter()
+	t.SetTitle("Handler Definitions")
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"Verbs", "Path"})
+
+	for _, handlerDefinition := range handlerDefinitions {
+		metaData := handlerDefinition.MetaData
+		httpVerbs, _ := metaData["httpVerbs"].([]contracts_handler.HTTPVERB)
+		verbBldr := strings.Builder{}
+
+		for idx, verb := range httpVerbs {
+			verbBldr.WriteString(verb.String())
+			if idx < len(httpVerbs)-1 {
+				verbBldr.WriteString(",")
+			}
+		}
+		path, _ := metaData["path"].(string)
+
+		t.AppendRow([]interface{}{verbBldr.String(), string(path)})
+
+	}
+	t.Render()
+	// Finally start the server
+	//----------------------------------------------------------------------------------
+	port := s.Startup.GetPort()
+	address := fmt.Sprintf(":%v", port)
+	if startupOptions != nil && startupOptions.Listener != nil {
+		// if we are here we are usually under test
+		s.e.Listener = startupOptions.Listener
+	}
+
+	err = s.e.Start(address)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to start server")
+	}
+	return err
+}
+func (s *Runtime) AddDefaultServices(builder *di.Builder) error {
+	contracts_container.AddContainerAccessorFunc(builder, s.GetContainer)
+	services_timeutils.AddTimeNow(builder)
+	services_timeutils.AddTimeParse(builder)
+	services_cookies.AddSingletonISecureCookie(builder)
+	services_contextaccessor.AddScopedIEchoContextAccessor(builder)
+	services_logger.AddILogger(builder)
+
+	return nil
+}
