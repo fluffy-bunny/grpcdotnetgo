f0bf9c621ebcff49e82910eceb885025d9ffe9c5
herb <ghstahl@gmail.com>
2022-03-31 09:47:47 -0700

    upfront config

--- pkg/contracts/core/types.go
+++ pkg/contracts/core/types.go
@@ -1,7 +1,10 @@
 package core
 
 type ConfigOptions struct {
-	Destination interface{}
-	RootConfig  []byte
-	ConfigPath  string
+	Destination            interface{}
+	RootConfig             []byte
+	ConfigPath             string
+	ApplicationEnvironment string `json:"applicationEnvironment" mapstructure:"APPLICATION_ENVIRONMENT"`
+	PrettyLog              bool   `json:"prettyLog" mapstructure:"PRETTY_LOG"`
+	LogLevel               string `json:"logLevel" mapstructure:"LOG_LEVEL"`
 }
