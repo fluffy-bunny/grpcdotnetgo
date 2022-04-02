6cc848c1ecb2a63a1c0862195910179e01964fe9
Herb <ghstahl@gmail.com>
2022-04-02 16:14:56 -0700

    wipe out all cookies if the session goes into panic

--- pkg/echo/middleware/session/ensure-sliding-session.go
+++ pkg/echo/middleware/session/ensure-sliding-session.go
@@ -1,8 +1,11 @@
 package session
 
 import (
+	"net/http"
+
 	di "github.com/fluffy-bunny/sarulabsdi"
 	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog/log"
 
 	contracts_session "github.com/fluffy-bunny/grpcdotnetgo/pkg/echo/contracts/session"
 )
@@ -11,6 +14,17 @@ import (
 func EnsureSlidingSession(_ di.Container, getSession contracts_session.GetSession) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
+			defer func() {
+				if err := recover(); err != nil {
+					log.Error().Msg("Panic in EnsureSlidingSession")
+					cookies := c.Cookies()
+					for _, cookie := range cookies {
+						cookie.MaxAge = -1
+						c.SetCookie(cookie)
+					}
+					c.Redirect(http.StatusFound, "/")
+				}
+			}()
 			sess := getSession(c)
 			if !sess.IsNew {
 				// we don't want to create a new session if nobody every created one before
