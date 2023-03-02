3e16676263a53d3939d3cbb94509bf43648fe326
Herb Stahl <herb@mapped.com>
2023-03-01 20:02:44 -0800

    add

--- pkg/contracts/auth/IClaimsValidator.go
+++ pkg/contracts/auth/IClaimsValidator.go
@@ -1,7 +1,6 @@
 package auth
 
 //go:generate genny   -pkg $GOPACKAGE        -in=../../../genny/sarulabsdi/interface-types.go -out=gen-$GOFILE gen "InterfaceType=IClaimsValidator"
-//go:generate mockgen -package=$GOPACKAGE -destination=../../mocks/$GOPACKAGE/mock_$GOFILE  go.mapped.dev/gtm/pkg/contracts/$GOPACKAGE IClaimsValidator
 import (
 	core_contracts_claimsprincipal "github.com/fluffy-bunny/grpcdotnetgo/pkg/contracts/claimsprincipal"
 )
