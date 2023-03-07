47fe61fb9e70497b459407b9b676d04308018ec5
ghstahl <ghstahl@users.noreply.github.com>
2023-03-07 18:09:10 +0000

    Apply genny and mockgen changes

--- example/internal/grpcContracts/helloworld/helloworld_di.pb.go
+++ example/internal/grpcContracts/helloworld/helloworld_di.pb.go
@@ -19,7 +19,7 @@ import (
 // is compatible with the grpc package it is being compiled against.
 const _ = pkg.SupportPackageIsVersion7
 
-func setNewField_wRUaCdK6RBQTovw9xRG9QmaDpZphiof8(dst interface{}, field string) {
+func setNewField_pqU7Z7o0VEYeEt0D8JBHj1PyWoMvAV4H(dst interface{}, field string) {
 	v := reflect.ValueOf(dst).Elem().FieldByName(field)
 	if v.IsValid() {
 		v.Set(reflect.New(v.Type().Elem()))
@@ -553,12 +553,12 @@ func Get_helloworldFullEmptyResponseFromFullMethodName(fullMethodName string) fu
 var helloworldFullMethodNameWithErrorResponseMap = map[string]func() interface{}{
 	"/example.internal.grpcContracts.helloworld.Greeter/SayHello": func() interface{} {
 		ret := &HelloReply{}
-		setNewField_wRUaCdK6RBQTovw9xRG9QmaDpZphiof8(ret, "Error")
+		setNewField_pqU7Z7o0VEYeEt0D8JBHj1PyWoMvAV4H(ret, "Error")
 		return ret
 	},
 	"/example.internal.grpcContracts.helloworld.Greeter2/SayHello": func() interface{} {
 		ret := &HelloReply2{}
-		setNewField_wRUaCdK6RBQTovw9xRG9QmaDpZphiof8(ret, "Error")
+		setNewField_pqU7Z7o0VEYeEt0D8JBHj1PyWoMvAV4H(ret, "Error")
 		return ret
 	},
 }
--- protoc-gen-go-di/helloworld/helloworld_di.pb.go
+++ protoc-gen-go-di/helloworld/helloworld_di.pb.go
@@ -19,7 +19,7 @@ import (
 // is compatible with the grpc package it is being compiled against.
 const _ = pkg.SupportPackageIsVersion7
 
-func setNewField_qlfUysU2YlaAPTJnZjrqAg3yO0dI1Mnm(dst interface{}, field string) {
+func setNewField_kGwoBm5DhJKhGUqOk90v8sMSDsLvC17l(dst interface{}, field string) {
 	v := reflect.ValueOf(dst).Elem().FieldByName(field)
 	if v.IsValid() {
 		v.Set(reflect.New(v.Type().Elem()))
@@ -553,12 +553,12 @@ func Get_helloworldFullEmptyResponseFromFullMethodName(fullMethodName string) fu
 var helloworldFullMethodNameWithErrorResponseMap = map[string]func() interface{}{
 	"/helloworld.Greeter/SayHello": func() interface{} {
 		ret := &HelloReply{}
-		setNewField_qlfUysU2YlaAPTJnZjrqAg3yO0dI1Mnm(ret, "Error")
+		setNewField_kGwoBm5DhJKhGUqOk90v8sMSDsLvC17l(ret, "Error")
 		return ret
 	},
 	"/helloworld.Greeter2/SayHello": func() interface{} {
 		ret := &HelloReply2{}
-		setNewField_qlfUysU2YlaAPTJnZjrqAg3yO0dI1Mnm(ret, "Error")
+		setNewField_kGwoBm5DhJKhGUqOk90v8sMSDsLvC17l(ret, "Error")
 		return ret
 	},
 }
