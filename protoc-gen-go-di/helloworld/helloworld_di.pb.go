39e4fd09bbab014ce33540274d71a86aa45d00bc
ghstahl <ghstahl@users.noreply.github.com>
2023-04-19 18:47:11 +0000

    Apply genny and mockgen changes

--- protoc-gen-go-di/helloworld/helloworld_di.pb.go
+++ protoc-gen-go-di/helloworld/helloworld_di.pb.go
@@ -19,7 +19,7 @@ import (
 // is compatible with the grpc package it is being compiled against.
 const _ = pkg.SupportPackageIsVersion7
 
-func setNewField_yTHDJSvpB214oOlkgc8BBQhUzIAkgyp9(dst interface{}, field string) {
+func setNewField_iHSMais74C6tdt2JSmUoskOz1yfVSfJM(dst interface{}, field string) {
 	v := reflect.ValueOf(dst).Elem().FieldByName(field)
 	if v.IsValid() {
 		v.Set(reflect.New(v.Type().Elem()))
@@ -553,12 +553,12 @@ func Get_helloworldFullEmptyResponseFromFullMethodName(fullMethodName string) fu
 var helloworldFullMethodNameWithErrorResponseMap = map[string]func() interface{}{
 	"/helloworld.Greeter/SayHello": func() interface{} {
 		ret := &HelloReply{}
-		setNewField_yTHDJSvpB214oOlkgc8BBQhUzIAkgyp9(ret, "Error")
+		setNewField_iHSMais74C6tdt2JSmUoskOz1yfVSfJM(ret, "Error")
 		return ret
 	},
 	"/helloworld.Greeter2/SayHello": func() interface{} {
 		ret := &HelloReply2{}
-		setNewField_yTHDJSvpB214oOlkgc8BBQhUzIAkgyp9(ret, "Error")
+		setNewField_iHSMais74C6tdt2JSmUoskOz1yfVSfJM(ret, "Error")
 		return ret
 	},
 }
