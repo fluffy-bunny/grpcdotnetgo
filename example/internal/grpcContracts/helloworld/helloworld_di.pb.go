d81cbdd3415a0963ad902c98868ffe6100125cd2
ghstahl <ghstahl@users.noreply.github.com>
2023-06-22 14:06:56 +0000

    Apply genny and mockgen changes

--- example/internal/grpcContracts/helloworld/helloworld_di.pb.go
+++ example/internal/grpcContracts/helloworld/helloworld_di.pb.go
@@ -19,7 +19,7 @@ import (
 // is compatible with the grpc package it is being compiled against.
 const _ = pkg.SupportPackageIsVersion7
 
-func setNewField_AFLl5pDngXDR1ATMho3e3XKmVKzWPhZu(dst interface{}, field string) {
+func setNewField_rtvOhPpa58Qo67LggzS87o4ScoaQIGgu(dst interface{}, field string) {
 	v := reflect.ValueOf(dst).Elem().FieldByName(field)
 	if v.IsValid() {
 		v.Set(reflect.New(v.Type().Elem()))
@@ -553,12 +553,12 @@ func Get_helloworldFullEmptyResponseFromFullMethodName(fullMethodName string) fu
 var helloworldFullMethodNameWithErrorResponseMap = map[string]func() interface{}{
 	"/example.internal.grpcContracts.helloworld.Greeter/SayHello": func() interface{} {
 		ret := &HelloReply{}
-		setNewField_AFLl5pDngXDR1ATMho3e3XKmVKzWPhZu(ret, "Error")
+		setNewField_rtvOhPpa58Qo67LggzS87o4ScoaQIGgu(ret, "Error")
 		return ret
 	},
 	"/example.internal.grpcContracts.helloworld.Greeter2/SayHello": func() interface{} {
 		ret := &HelloReply2{}
-		setNewField_AFLl5pDngXDR1ATMho3e3XKmVKzWPhZu(ret, "Error")
+		setNewField_rtvOhPpa58Qo67LggzS87o4ScoaQIGgu(ret, "Error")
 		return ret
 	},
 }
